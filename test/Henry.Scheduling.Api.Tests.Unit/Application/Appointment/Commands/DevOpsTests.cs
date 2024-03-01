9abf5f891ffa34a030cb773fb7474665c3eb98d3
Randall Sexton <jrandallsexton@gmail.com>
2024-03-01 13:34:22 -0500

    bogus tests

---
+++ test/Henry.Scheduling.Api.Tests.Unit/Application/Appointment/Commands/DevOpsTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace Henry.Scheduling.Api.Tests.Unit.Application.Appointment.Commands
+{
+    public class DevOpsTests
+    {
+        [Fact]
+        public void PipelineTest()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
